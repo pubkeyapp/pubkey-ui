5b3f36c0dfa18f8259229ffcb3aa4b5e06c1387c
Bram Borggreve <beeman@beeman.nl>
2023-12-03 05:40:03 +0000

    refactor: use Paper to back our card

--- packages/core/src/lib/ui-card/ui-card.tsx
+++ packages/core/src/lib/ui-card/ui-card.tsx
@@ -1,9 +1,9 @@
-import { Box, Card, CardProps, Skeleton } from '@mantine/core'
+import { Box, Paper, PaperProps, Skeleton } from '@mantine/core'
 import { useUiBreakpoints } from '../ui-theme'
 import { ReactNode } from 'react'
 import { UiCardTitle } from './ui-card-title'
 
-interface UiCardProps extends CardProps {
+interface UiCardProps extends PaperProps {
   children: ReactNode
   loading?: boolean
   title?: ReactNode
@@ -13,11 +13,11 @@ export function UiCard({ loading, title, ...props }: UiCardProps) {
   const { isSm } = useUiBreakpoints()
 
   return (
-    <Card p={isSm ? 'xs' : 'md'} withBorder {...props}>
+    <Paper p={isSm ? 'xs' : 'md'} withBorder {...props}>
       {title ? (
         <Box mb={isSm ? 'xs' : 'md'}>{typeof title === 'string' ? <UiCardTitle>{title}</UiCardTitle> : title}</Box>
       ) : null}
       {loading ? <Skeleton visible={loading}>{props.children}</Skeleton> : props.children}
-    </Card>
+    </Paper>
   )
 }
