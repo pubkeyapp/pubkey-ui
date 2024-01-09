8ba66a3bf603fad9a1dbc34325fc2cabc1b6061e
Bram Borggreve <beeman@beeman.nl>
2024-01-09 23:30:50 +0000

    fix: disable crypto in config.resolve.fallback

--- apps/web/webpack.config.js
+++ apps/web/webpack.config.js
@@ -6,5 +6,8 @@ module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
   config.ignoreWarnings = [/Failed to parse source map/]
+  config.resolve.fallback = {
+    crypto: false,
+  }
   return config
 })
